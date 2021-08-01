2e88227ab0eaa69ff284505979d1751af503290d
수호 <rosua328@gmail.com>
2021-08-01 21:18:16 +0900

    final
    final

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ searchInputEl.setAttribute('placeholder', '');
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function(){
 
@@ -26,14 +27,32 @@ window.addEventListener('scroll', _.throttle(function(){
       opacity: 0,
       display: 'none'
     });
+
+gsap.to(toTopEl, 0.2, {
+ x: 0 
+});
+
   }
   else{
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     });
+
+    gsap.to(toTopEl, 0.2, {
+      x: 100
+     });
+
   }
 }, 300));
+
+
+
+toTopEl.addEventListener('click', function(){
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+})
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function(fadeEl, index){
@@ -67,6 +86,17 @@ new Swiper('.promotion .swiper-container', {
   }
 });
 
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loog: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation:{
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
@@ -96,15 +126,19 @@ gsap.to(selector, random(1.5, 2.5), {
 }
 
 floatingObject('.floating1', 1, 15);
-floatingObject('.floating1', .5, 15);
-floatingObject('.floating1', 1.5, 20);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
 
 const spyEls = document.querySelectorAll('section.scroll-spy');
 spyEls.forEach(function(spyEl){
 new ScrollMagic
 .Scene({
-triggerElement: spyEl
+triggerElement: spyEl,
+triggerHook: .8
 })
-.setClassToggle()
-.addTo();
+.setClassToggle(spyEl, 'show')
+.addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
